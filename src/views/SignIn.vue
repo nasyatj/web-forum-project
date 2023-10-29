7fe13ca91ada58c0d747ec1cecbb527e8c7202a5
h12s <76884971+h12s@users.noreply.github.com>
2023-10-29 18:18:04 -0400

    Update SignIn.vue

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,13 +1,73 @@
 <template>
-	<div class="sign-in">
-		<h1>Sign-in Page</h1>
-	</div>
+	<h2>Create your account</h2>
+
+	<form @submit.prevent="handleSubmit">
+		<label>Email:</label>
+		<input type="email" required v-model="email">
+
+		<label>Username:</label>
+		<input type="text" required v-model="username">
+
+		<label>Password:</label>
+		<input type="password" required v-model="password">
+
+		<label>Confirm Password:</label>
+		<input type="password" required v-model="confirmPassword">
+
+		<button type="submit">Submit</button>
+	</form>
 </template>
 
 <script>
-	
+	import { db } from '@/firebase';
+	import { collection, query, where, getDocs } from "firebase/firestore"; 
+
+	export default {
+		data() {
+			return {
+				email: '',
+				username: '',
+				password: '',
+				confirmPassword: '',
+			}
+		},
+		methods: {
+			async handleSubmit() {
+				if (this.password != this.confirmPassword) {
+					alert('Passwords do not match, please try again');
+					return;
+				}
+
+				const usersTable = collection(db, "users");
+				let q = query(usersTable, where('username', '==', this.username));
+				const querySnapshot = await getDocs(q);
+				querySnapshot.forEach(doc => {
+					console.log('document');
+					console.log(doc.id, '=>', doc.data());
+				});
+
+				q = query(usersTable, where('email', '==', this.email));
+
+			}
+		}
+	};
 </script>
 
-<style>
+<style scoped>
+	form label, form input {
+		display: block;
+	}
 
+	form input {
+		margin: 0 auto 20px auto;
+	}
+
+	form label {
+		margin: 0 auto 10px auto;
+	}
+
+	button {
+		padding: 10px;
+		margin: 10px;
+	}
 </style>
