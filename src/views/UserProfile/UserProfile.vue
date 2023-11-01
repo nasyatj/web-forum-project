c116df919978e363c986b68cc6b21ec81f53598a
h12s <76884971+h12s@users.noreply.github.com>
2023-11-01 16:36:54 -0400

    Add user-profile

---
+++ src/views/UserProfile/UserProfile.vue
@@ -0,0 +1,39 @@
+<template>
+	<h2>Edit your user profile</h2>
+
+    <nav>
+        <router-link to="/user-profile/basic-info">Basic Info</router-link>
+		<router-link to="/user-profile/change-password">Password</router-link>
+		<router-link to="/user-profile/user-posts">Posts</router-link>
+    </nav>
+
+    <router-view 
+        v-bind:loggedInUsername="loggedInUsername"
+        @updateUsername="updateParentWithNewUsername"
+    />
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        props: [
+            'loggedInUsername'
+        ],
+        methods: {
+            updateParentWithNewUsername(newUsername) {
+                this.$emit('updateUsername', newUsername);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+	nav {
+        display: inline-block;
+        margin: 20px auto 20px auto;
+    }
+</style>
