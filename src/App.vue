aa799e3b982b86b52613637a49d057b7d6b08d76
Nissi Chima <nissichima@yahoo.com>
2023-11-23 15:08:39 -0500

    background css

--- src/App.vue
+++ src/App.vue
@@ -354,7 +354,7 @@ import { tSThisType } from '@babel/types';
         display: flex;
 	padding-right: 20px; 
         border-bottom: 1px solid #FF7F50; /* Coral Border */
-        background-color: #A9A9A9;
+        background-color: black;
         color: #000; /* black Text */
     }
 
