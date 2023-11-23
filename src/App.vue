ff5e606e32976d19b97dd1ab0ce59b0a58457b50
Nissi Chima <nissichima@yahoo.com>
2023-11-23 15:19:10 -0500

    background css

--- src/App.vue
+++ src/App.vue
@@ -328,7 +328,7 @@ import { tSThisType } from '@babel/types';
     #app {
         font-family: Avenir, Helvetica, Arial, sans-serif;
         text-align: center;
-        color: #008080; /* Teal Text */
+        color: #000;
         background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);  
 	background-size: 400% 400%;
 	animation: gradientBG 15s ease infinite;
