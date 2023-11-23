3171b60534cb17f8a732dd8b0816d4eb992ab566
Nissi Chima <nissichima@yahoo.com>
2023-11-23 15:10:31 -0500

    background css

--- src/App.vue
+++ src/App.vue
@@ -373,7 +373,7 @@ import { tSThisType } from '@babel/types';
 
    h2 {
 	font-size:30px;
-	color: #fff;
+	color: #000;
 	font-family: Baskerville, 'Baskerville Old Face', 'Hoefler Text', Garamond, 'Times New Roman', serif;
 	}
 
