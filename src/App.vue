1ca605ec4e2009b6b4587790676848dd6b16a498
Nissi Chima <nissichima@yahoo.com>
2023-11-23 11:09:44 -0500

    fixing header css

--- src/App.vue
+++ src/App.vue
@@ -374,7 +374,7 @@ import { tSThisType } from '@babel/types';
     }
 
 	header h1 {
-  	 font-size: 52px;
+  	 font-size:42px;
   	margin-left: 20px; /* Or as much as needed to move it to the right */
   
 }
